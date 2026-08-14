2c8f2ddc23e6abe8c71ff7af5a4831ccd9a43cef
Hung Truong Van <xtruongvanhung@gmail.com>
2026-08-14 15:32:14 +0700

    TWD-1 Complete project setup and JUnit configuration

---
+++ src/test/java/com/testworldcup/testworldcuptdd/ProjectSetupTest.java
@@ -0,0 +1,12 @@
+package com.testworldcup.testworldcuptdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProjectSetupTest {
+
+    @Test
+    void junitShouldRun() {
+        assertTrue(true);
+    }
+}
