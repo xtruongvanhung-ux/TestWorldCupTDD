f00a99288b5cadad89a23bb7f218155076a43192
Hung Truong Van <xtruongvanhung@gmail.com>
2026-08-14 14:50:53 +0700

    Initial commit

---
+++ src/main/java/com/testworldcup/testworldcuptdd/TestWorldCupTDD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.testworldcup.testworldcuptdd;
+
+/**
+ *
+ * @author Admin
+ */
+public class TestWorldCupTDD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
